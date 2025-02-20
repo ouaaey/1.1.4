b4624a44e57ffa012d612bea1213e11941f24dd0
Irina <ouaaey@gmail.com>
2025-02-20 13:03:53 +0400

    на гитХаб1!

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,16 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/mydatabase";
+    private static final String USERNAME = "root"; // имя пользователя воркбенча
+    private static final String PASSWORD = "wErFg45!!!"; // пароль, соответственно
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
 }
